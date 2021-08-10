97dc2d7fc075e6a9d0475d02e5a9f4286a2b4eba
Christian Decker <decker.christian@gmail.com>
2021-08-10 18:36:55 +0200

    rust: Add identities and network type

--- libs/rust/src/lib.rs
+++ libs/rust/src/lib.rs
@@ -1,4 +1,46 @@
 pub use libhsmd_sys::Hsmd;
 
+extern crate anyhow;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate log;
+
 mod pb;
 pub mod signer;
+
+pub enum Network {
+    BITCOIN,
+    TESTNET,
+    REGTEST,
+}
+
+impl Into<&'static str> for Network {
+    fn into(self: Network) -> &'static str {
+        match self {
+            Network::BITCOIN => "bitcoin",
+            Network::TESTNET => "testnet",
+            Network::REGTEST => "regtest",
+        }
+    }
+}
+
+pub mod tls {
+    use tonic::transport::{Certificate, ClientTlsConfig, Identity};
+    lazy_static! {
+        static ref CA: Certificate = Certificate::from_pem(include_str!("../../../tls/ca.pem"));
+        static ref NOBODY: Identity = Identity::from_pem(
+            include_str!("../../../tls/users-nobody.pem"),
+            include_str!("../../../tls/users-nobody-key.pem")
+        );
+        pub static ref NOBODY_CONFIG: ClientTlsConfig = ClientTlsConfig::new()
+            .domain_name("localhost")
+            .ca_certificate(Certificate::from_pem(include_str!("../../../tls/ca.pem")))
+            .identity(Identity::from_pem(
+                include_str!("../../../tls/users-nobody.pem"),
+                include_str!("../../../tls/users-nobody-key.pem")
+            ));
+    }
+}
