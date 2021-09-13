5966394388f2f706660a7bfa01dbfb0ef321f131
Christian Decker <decker.christian@gmail.com>
2021-09-13 12:42:16 +0200

    rs: Implement a layer to cache and sign outgoing requests

--- libs/rust/src/lib.rs
+++ libs/rust/src/lib.rs
@@ -33,14 +33,14 @@ pub mod node {
     use crate::pb::node_client::NodeClient;
     use crate::pb::{scheduler_client::SchedulerClient, ScheduleRequest};
     use anyhow::{anyhow, Result};
-    use service::AuthService;
     use tonic::transport::{Certificate, Channel, ClientTlsConfig, Identity, Uri};
     use tower::ServiceBuilder;
 
-    type Client = NodeClient<service::AuthSvc>;
+    type Client = NodeClient<service::AuthService>;
 
     /// A builder is a synchronous mechanism to configure a NodeStub
     /// before actually connecting to it asynchronously.
+    #[allow(dead_code)]
     pub struct Builder {
         node_id: Vec<u8>,
         network: Network,
@@ -79,13 +79,23 @@ pub mod node {
 
         pub async fn connect(self, node_uri: String) -> Result<Client> {
             let node_uri = Uri::from_maybe_shared(node_uri)?;
+
+            let layer = match self.private_key {
+                Some(k) => service::AuthLayer::new(k),
+                None => {
+                    return Err(anyhow!(
+                        "Cannot connect a node::Client without first configuring its identity"
+                    ))
+                }
+            };
+
             let chan: tonic::transport::Channel = Channel::builder(node_uri)
                 .tls_config(self.client_tls)?
                 .connect()
                 .await?;
 
             let chan = ServiceBuilder::new()
-                .layer_fn(service::AuthSvc::new)
+                .layer(layer)
                 .service(chan);
 
             Ok(NodeClient::new(chan))
@@ -130,21 +140,32 @@ pub mod node {
         use tonic::transport::Channel;
         use tower::{Layer, Service};
 
+        pub struct AuthLayer {
+            hmac_key: Vec<u8>,
+        }
+
+        impl AuthLayer {
+            pub fn new(hmac_key: Vec<u8>) -> Self {
+                AuthLayer { hmac_key: hmac_key }
+            }
+        }
+
+        impl Layer<Channel> for AuthLayer {
+            type Service = AuthService;
+
+            fn layer(&self, inner: Channel) -> Self::Service {
+                AuthService {
+                    hmac_key: self.hmac_key.clone(),
+                    inner,
+                }
+            }
+        }
+
         pub struct AuthService {
             hmac_key: Vec<u8>,
             inner: Channel,
         }
-        pub struct AuthSvc {
-            inner: Channel,
-        }
-
-        impl AuthSvc {
-            pub fn new(inner: Channel) -> Self {
-                AuthSvc { inner }
-            }
-        }
-
-        impl Service<Request<BoxBody>> for AuthSvc {
+        impl Service<Request<BoxBody>> for AuthService {
             type Response = Response<Body>;
             type Error = Box<dyn std::error::Error + Send + Sync>;
             #[allow(clippy::type_complexity)]
@@ -153,24 +174,21 @@ pub mod node {
             fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
                 self.inner.poll_ready(cx).map_err(Into::into)
             }
-
-            fn call(&mut self, req: Request<BoxBody>) -> Self::Future {
+            fn call(&mut self, request: Request<BoxBody>) -> Self::Future {
                 // This is necessary because tonic internally uses `tower::buffer::Buffer`.
                 // See https://github.com/tower-rs/tower/issues/547#issuecomment-767629149
                 // for details on why this is necessary
                 let clone = self.inner.clone();
                 let mut inner = std::mem::replace(&mut self.inner, clone);
+                let key = self.hmac_key.clone();
                 Box::pin(async move {
                     use tonic::codegen::Body;
-		    let mut request = req;
-
                     let (parts, mut body) = request.into_parts();
-                    //let mut data = body.data().await.unwrap().unwrap();
-                    //trace!("Got request {:?}", &data);
 
-                    //futures_util::pin_mut!(body);
-                    //let body = tonic::body::BoxBody::new(body);
+                    let data = dbg!(body.data().await.unwrap().unwrap());
+                    trace!("Got request {:?}, signing with {:?}", data, key);
 
+                    let body = crate::node::stasher::StashBody::new(data).into();
                     let request = Request::from_parts(parts, body);
 
                     // Do extra async work here...
@@ -178,6 +196,59 @@ pub mod node {
                     trace!("Response!");
                     Ok(response)
                 })
+            }
+        }
+    }
+    pub mod stasher {
+        use bytes::Bytes;
+        use http::HeaderMap;
+        use http_body::Body;
+        use pin_project::pin_project;
+        use std::{
+            pin::Pin,
+            task::{Context, Poll},
+        };
+        use tonic::body::BoxBody;
+        use tonic::Status;
+
+        #[pin_project]
+        #[derive(Debug)]
+        pub(crate) struct StashBody {
+            value: Option<Bytes>,
+        }
+
+        impl StashBody {
+            pub(crate) fn new(val: Bytes) -> Self {
+                Self { value: Some(val) }
+            }
+        }
+
+        impl Body for StashBody {
+            type Data = Bytes;
+            type Error = Status;
+
+            fn is_end_stream(&self) -> bool {
+                self.value.is_none()
+            }
+
+            fn poll_data(
+                self: Pin<&mut Self>,
+                _cx: &mut Context<'_>,
+            ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
+                Poll::Ready(self.project().value.take().map(|v| Ok(v)))
+            }
+
+            fn poll_trailers(
+                self: Pin<&mut Self>,
+                _cx: &mut Context<'_>,
+            ) -> Poll<Result<Option<HeaderMap>, Status>> {
+                Poll::Ready(Ok(None))
+            }
+        }
+
+        impl From<StashBody> for BoxBody {
+            fn from(v: StashBody) -> BoxBody {
+                BoxBody::new(v)
             }
         }
     }
