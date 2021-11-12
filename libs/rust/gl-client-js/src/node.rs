62d7b5cf3249c0136d9d864173f65e8845fb9618
Christian Decker <cdecker@mac.local>
2021-11-12 12:59:03 +0100

    js: Add missing shims in rust-land

--- libs/rust/gl-client-js/src/node.rs
+++ libs/rust/gl-client-js/src/node.rs
@@ -52,6 +52,49 @@ impl Node {
                     .await?
                     .into_inner(),
             ),
+            "connect_peer" => convert(
+                client
+                    .connect_peer(pb::ConnectRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "disconnect" => convert(
+                client
+                    .disconnect(pb::DisconnectRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "newaddr" => convert(
+                client
+                    .new_addr(pb::NewAddrRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "fund_channel" => convert(
+                client
+                    .fund_channel(pb::FundChannelRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "create_invoice" => convert(
+                client
+                    .create_invoice(pb::InvoiceRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "pay" => convert(client.pay(pb::PayRequest::decode(req)?).await?.into_inner()),
+            "withdraw" => convert(
+                client
+                    .withdraw(pb::WithdrawRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
+            "keysend" => convert(
+                client
+                    .keysend(pb::KeysendRequest::decode(req)?)
+                    .await?
+                    .into_inner(),
+            ),
             o => Err(anyhow!(
                 "method \"{}\" is unknown to the glclient library",
                 o
@@ -64,11 +107,15 @@ impl Node {
         let method = cx.argument::<JsString>(1)?.value(&mut cx);
         let buf = cx.argument::<JsBuffer>(2)?;
         let args: Vec<u8> = cx.borrow(&buf, |data| data.as_slice().to_vec());
-        let res = exec(this.dispatch(method.as_ref(), &args)).unwrap();
 
-        let jsbuf = JsBuffer::new(&mut cx, res.len() as u32)?;
-        cx.borrow(&jsbuf, |jsbuf| jsbuf.as_mut_slice().copy_from_slice(&res));
-        Ok(jsbuf)
+        match exec(this.dispatch(method.as_ref(), &args)) {
+            Ok(res) => {
+                let jsbuf = JsBuffer::new(&mut cx, res.len() as u32)?;
+                cx.borrow(&jsbuf, |jsbuf| jsbuf.as_mut_slice().copy_from_slice(&res));
+                Ok(jsbuf)
+            }
+            Err(e) => cx.throw_error(format!("error calling {}: {}", method, e))?,
+        }
     }
 }
 
