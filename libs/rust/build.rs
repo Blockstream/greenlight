d97aa8a2f19970f8597371c6e42f4b195abacd10
Christian Decker <decker.christian@gmail.com>
2021-11-22 16:07:48 +0100

    rs: Move the proto files into the gl-client directory
    Builds from source will checkout only that subdirectory, so make sure
    the proto files are included.

--- libs/rust/build.rs
+++ libs/rust/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    tonic_build::compile_protos("../proto/greenlight.proto").unwrap();
-    tonic_build::compile_protos("../proto/scheduler.proto").unwrap();
+    tonic_build::compile_protos("proto/greenlight.proto").unwrap();
+    tonic_build::compile_protos("proto/scheduler.proto").unwrap();
 }
