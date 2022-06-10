7665e53e07c243503d584e83560f765e80dbe5fe
Riccardo Casatta <riccardo@casatta.it>
2022-06-10 13:11:58 +0200

    Move gl-client in own directory
    /libs/Cargo.toml remain only to define the workspace, this way is possible from
    rust to use gl-client alone.

--- libs/gl-client/src/tls.rs
+++ libs/gl-client/src/tls.rs
@@ -1,9 +1,9 @@
 use anyhow::{Context, Result};
 use tonic::transport::{Certificate, ClientTlsConfig, Identity};
 
-const CA_RAW: &[u8] = include_str!("../tls/ca.pem").as_bytes();
-const NOBODY_CRT: &[u8] = include_str!("../tls/users-nobody.pem").as_bytes();
-const NOBODY_KEY: &[u8] = include_str!("../tls/users-nobody-key.pem").as_bytes();
+const CA_RAW: &[u8] = include_str!("../../tls/ca.pem").as_bytes();
+const NOBODY_CRT: &[u8] = include_str!("../../tls/users-nobody.pem").as_bytes();
+const NOBODY_KEY: &[u8] = include_str!("../../tls/users-nobody-key.pem").as_bytes();
 
 /// In order to allow the clients to talk to the
 /// [`crate::scheduler::Scheduler`] a default certificate and private
