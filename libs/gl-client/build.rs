0a4ba6200ee7aac313d6fcb21a2cf8f5406f7de9
Riccardo Casatta <riccardo@casatta.it>
2022-06-23 15:52:44 +0200

    Add Serde for greenlight protocol messages objects

--- libs/gl-client/build.rs
+++ libs/gl-client/build.rs
@@ -1,4 +1,59 @@
 fn main() {
-    tonic_build::compile_protos("../proto/greenlight.proto").unwrap();
-    tonic_build::compile_protos("../proto/scheduler.proto").unwrap();
+    let mut builder = tonic_build::configure();
+    for object in OBJECTS {
+        builder = builder.type_attribute(object, ATTR);
+    }
+
+    builder
+        .compile(&["../proto/greenlight.proto", "../proto/scheduler.proto"], &["../proto"])
+        .unwrap();
 }
+
+const ATTR: &'static str = "#[derive(serde::Serialize, serde::Deserialize)]";
+const OBJECTS: [&'static str; 45] = [
+    "ConnectRequest",
+    "ConnectResponse",
+    "DisconnectRequest",
+    "DisconnectResponse",
+    "Outpoint",
+    "Confirmation",
+    "Amount.unit",
+    "Amount",
+    "FundChannelRequest",
+    "FundChannelResponse",
+    "Address",
+    "Feerate",
+    "Feerate.value",
+    "GetInfoRequest",
+    "GetInfoResponse",
+    "Invoice",
+    "InvoiceRequest",
+    "KeysendRequest",
+    "TlvField",
+    "Routehint",
+    "RoutehintHop",
+    "ListFundsRequest",
+    "ListFundsResponse",
+    "ListFundsChannel",
+    "ListFundsOutput",
+    "ListInvoicesRequest",
+    "InvoiceIdentifier",
+    "InvoiceIdentifier.id",
+    "ListInvoicesResponse",
+    "ListPaymentsRequest",
+    "PaymentIdentifier",
+    "PaymentIdentifier.id",
+    "ListPaymentsResponse",
+    "Payment",
+    "ListPeersRequest",
+    "ListPeersResponse",
+    "Peer",
+    "Channel",
+    "Htlc",
+    "NewAddrRequest",
+    "NewAddrResponse",
+    "PayRequest",
+    "Payment",
+    "WithdrawRequest",
+    "WithdrawResponse",
+];
