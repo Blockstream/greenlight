3158a81a56a0552d252376abcdfc2d0538be097d
Peter Neuroth <pet.v.ne@gmail.com>
2022-07-27 23:53:16 +0200

    tests: Move fixtures to a separate file
    We want to use the fixtures in multiple files.

--- libs/gl-client-py/tests/test_scheduler.py
+++ libs/gl-client-py/tests/test_scheduler.py
@@ -1,38 +1,8 @@
 from gltesting.fixtures import *
+from fixtures import *
 from glclient import TlsConfig, Signer, Scheduler, Node
 from binascii import hexlify
 import unittest
-
-
-@pytest.fixture
-def tls(nobody_id):
-    """Just a preconfigured TlsConfig.
-    """
-    return TlsConfig().with_ca_certificate(
-        nobody_id.caroot
-    ).identity(
-        nobody_id.cert_chain,
-        nobody_id.private_key
-    )
-
-
-@pytest.fixture
-def signer(scheduler, tls):
-    secret = b'\x00'*32
-    network='regtest'
-    return Signer(secret, network=network, tls=tls)
-
-
-@pytest.fixture
-def sclient(signer, tls):
-    """Just a preconfigured scheduler client.
-
-    This scheduler client is configured with a secret for easy
-    registration and recovery, but no mTLS certificate yet.
-
-    """
-    network = 'regtest'
-    return Scheduler(signer.node_id(), network=network, tls=tls)
 
 
 def test_connect(scheduler, tls):
