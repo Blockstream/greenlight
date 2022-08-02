718343b0b4d14f433c62a291f05686472248395d
Christian Decker <decker.christian@gmail.com>
2022-08-02 19:08:49 +0200

    gltesting: Add scheduler implementation
    This is a mock implementation of the scheduler, limited to running
    nodes locally, but for testing it is more than adequate.

---
+++ libs/gl-testing/gltesting/utils.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import Enum
+
+@dataclass
+class NodeVersion:
+    # The path that we should use when calling `lightningd`, it is
+    # version specific
+    path: str
+    # The stringified version number
+    name: str
+
+
+@dataclass
+class SignerVersion:
+    name: str
+
+    def is_compat(self, nv: NodeVersion) -> bool:
+        """Whether or not a signer is compatible with a given NodeVersion."""
+        compat = {
+            "v0.10.1": ["v0.10.1"],
+            "v0.10.2": ["v0.10.2"],
+            "v0.11.0.1": ["v0.11.0.1", "v0.11.2gl2"],
+            "v0.11.2": ["v0.11.0.1", "v0.11.2gl2"],
+        }
+
+        return self.name in compat[nv.name]
+
+
+class Network(Enum):
+    """Supported networks."""
+
+    BITCOIN = 0
+    TESTNET = 1
+    REGTEST = 2
