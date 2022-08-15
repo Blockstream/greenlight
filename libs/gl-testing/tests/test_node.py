4748ba20bac6ea6fcc4dbfafbb211fcbd36ac46f
Christian Decker <decker.christian@gmail.com>
2022-08-15 17:44:22 +0200

    testing: Configure glclient to connect to the scheduled node
    This completes the first step, we can now perform operations that do
    not require a signer. That's next.

--- libs/gl-testing/tests/test_node.py
+++ libs/gl-testing/tests/test_node.py
@@ -2,9 +2,22 @@ from gltesting.identity import Identity
 from gltesting.fixtures import *
 from rich.pretty import pprint
 
-def test_node_start(scheduler, clients, bitcoind):
+def test_node_start(scheduler, clients):
     c = clients.new()
     res = c.register(configure=True)
     pprint(res)
 
     node_info = c.scheduler().schedule()
+    pprint(node_info)
+    assert node_info.grpc_uri is not None
+
+
+def test_node_connect(scheduler, clients, bitcoind):
+    """Register and schedule a node, then connect to it.
+    """
+    c = clients.new()
+    c.register(configure=True)
+    n = c.node()
+    info = n.get_info()
+    pprint(info)
+
