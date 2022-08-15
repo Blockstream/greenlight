3fffd16728bd9d68117eecd60b0711e556856eac
Christian Decker <decker.christian@gmail.com>
2022-08-15 17:46:55 +0200

    testing: Add signer waiting support to the scheduler
    Now we can sign incoming requests.

--- libs/gl-testing/tests/test_node.py
+++ libs/gl-testing/tests/test_node.py
@@ -1,6 +1,7 @@
 from gltesting.identity import Identity
 from gltesting.fixtures import *
 from rich.pretty import pprint
+from glclient import nodepb
 
 def test_node_start(scheduler, clients):
     c = clients.new()
@@ -20,4 +21,23 @@ def test_node_connect(scheduler, clients, bitcoind):
     n = c.node()
     info = n.get_info()
     pprint(info)
+
+
+def test_node_signer(clients, executor):
+    """Ensure we can attach a signer to the node and sign an invoice.
+    """
+    c = clients.new()
+    c.register(configure=True)
+    n = c.node()
+
+    # Running the `invoice` invocation in a separate thread since
+    # it'll block until the signer connects.
+    fi = executor.submit(n.create_invoice, 'test', nodepb.Amount(millisatoshi=42000))
+
+    # Now attach the signer and the above call should return
+    h = c.signer().run_in_thread()
+
+    inv = fi.result(10)
+    pprint(inv)
+    h.shutdown()
 
