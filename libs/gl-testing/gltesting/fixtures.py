5126e649eab360f19746d0e7825a4de899e972b2
Christian Decker <decker.christian@gmail.com>
2022-08-15 19:22:07 +0200

    testing: Add payment test with larger network

--- libs/gl-testing/gltesting/fixtures.py
+++ libs/gl-testing/gltesting/fixtures.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 import logging
 import sys
-from pyln.testing.fixtures import bitcoind, teardown_checks
+from pyln.testing.fixtures import bitcoind, teardown_checks, node_factory, node_cls, test_name, executor, db_provider, test_base_dir, throttler, jsonschemas
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
