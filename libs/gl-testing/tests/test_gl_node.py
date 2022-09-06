66456004d74390c42760d8e458214980b6d63610
Riccardo Casatta <riccardo@casatta.it>
2022-09-06 16:08:36 +0200

    test: new test funding channel from greenlight

---
+++ libs/gl-testing/tests/test_gl_node.py
@@ -0,0 +1,35 @@
+from gltesting.identity import Identity
+from gltesting.fixtures import *
+from pyln.testing.utils import wait_for
+from rich.pretty import pprint
+from glclient import nodepb
+import time
+
+def test_node_network_gl_fund(node_factory, clients, bitcoind):
+    """Setup a small network and check that we can send/receive payments.
+
+    ```dot
+    l1 -> l2 -> gl1
+    ```
+    """
+    l1, l2 = node_factory.line_graph(2)
+    c = clients.new()
+    c.register(configure=True)
+    gl1 = c.node()
+
+    # Handshake needs signer for ECDH of Noise_XK exchange
+    s = c.signer().run_in_thread()
+    gl1.connect_peer(l2.info['id'], f'127.0.0.1:{l2.daemon.port}')
+    gl1.connect_peer(l1.info['id'], f'127.0.0.1:{l1.daemon.port}')
+
+    # Fund gl1
+    gl1_address = gl1.new_address().address
+    bitcoind.rpc.sendtoaddress(gl1_address, 1)
+    bitcoind.generate_block(1, wait_for_mempool=1)
+    wait_for(lambda: len(gl1.list_funds().outputs) > 0 )
+
+    # Now open a channel from gl1 -> l2
+    l1_node_id = l1.rpc.getinfo()['id']
+    channel_result = gl1.fund_channel(l1_node_id, nodepb.Amount(millisatoshi=50000000))
+    bitcoind.generate_block(1, wait_for_mempool=1)
+
