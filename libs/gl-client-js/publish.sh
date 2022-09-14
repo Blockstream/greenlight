6b225c52efbb89fc9d42cb17d7af9b91f258d46f
Christian Decker <decker.christian@gmail.com>
2022-09-14 15:14:29 +0200

    js: Add Makefile targets to package and publish

--- libs/gl-client-js/publish.sh
+++ libs/gl-client-js/publish.sh
@@ -1,30 +1,19 @@
-#!/bin/bash
-
+#!/bin/bash -x
+set -e
 # We need to publish in a temporary directory since we're going to
 # rewrite a bunch of paths and directories.
 tmpfile=$(mktemp -d /tmp/gl-client-js.XXXXXX)
 echo "$tmpfile"
-FILES=$(jq '.files[]' -r package.json | grep -v index.node)
+FILES=$(jq '.files[]' -r package.json | grep -vE '(index.node|libhsmd-sys-rs|gl-client)')
 cp -R .npmrc $FILES "${tmpfile}"
 sed -i 's/gl-client = { path = ".." }/gl-client = { path = "gl-client" }/g' "${tmpfile}/Cargo.toml"
 echo '[workspace]' >> "${tmpfile}/Cargo.toml"
 
 # Now vendor the crate we depend on, but stripping its workspace entry:
-mkdir "${tmpfile}/gl-client"
-cp ../Cargo.toml "${tmpfile}/gl-client/"
-cp ../build.rs "${tmpfile}/gl-client/"
-cp -R ../src "${tmpfile}/gl-client/"
-cp -R ../tls "${tmpfile}/gl-client/"
-cp -R ../proto "${tmpfile}/gl-client/"
+cp -r ../gl-client "${tmpfile}/"
 sed -i '/\[workspace\]/Q' "${tmpfile}/gl-client/Cargo.toml"
 
-mkdir "${tmpfile}/libhsmd-sys-rs"
-
-cp ../libhsmd-sys-rs/Cargo.toml "${tmpfile}/libhsmd-sys-rs/"
-cp ../libhsmd-sys-rs/build.rs "${tmpfile}/libhsmd-sys-rs/"
-cp ../libhsmd-sys-rs/libhsmd.[ch] "${tmpfile}/libhsmd-sys-rs/"
-cp ../libhsmd-sys-rs/shims.c "${tmpfile}/libhsmd-sys-rs/"
-cp -R ../libhsmd-sys-rs/src "${tmpfile}/libhsmd-sys-rs/"
+cp -r ../libhsmd-sys-rs/ "${tmpfile}/"
 sed -i 's|libhsmd-sys = { path = "libhsmd-sys-rs" }|libhsmd-sys = { path = "../libhsmd-sys-rs" }|g' "${tmpfile}/gl-client/Cargo.toml"
 
 (
