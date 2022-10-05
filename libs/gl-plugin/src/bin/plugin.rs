5d534b7282dd1794c6f169605a7f7bf6256bee21
Christian Decker <decker.christian@gmail.com>
2022-10-05 17:33:39 +0200

    plugin: Add local state signer storage
    This storage is meant to be the authoritative copy that we send to
    signers with requests, and that gets update when signers respond to
    requests. The `SledStateStore` allows us to store the signer state
    alongside the node itself in its datadir.

--- libs/gl-plugin/src/bin/plugin.rs
+++ libs/gl-plugin/src/bin/plugin.rs
@@ -5,8 +5,17 @@ use std::env;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let cwd = env::current_dir()?;
+
+    // We use the SledStatestore since we own the directory and we can
+    // rely on the state being persisted correctly here.
+
+    let mut state_dir = cwd.clone();
+    state_dir.push("signer_state");
+
+    let state_store = gl_plugin::storage::SledStateStore::new(state_dir)?;
+
     info!("Running in {}", cwd.to_str().unwrap());
-    let plugin = gl_plugin::init()?;
+    let plugin = gl_plugin::init(Box::new(state_store)).await?;
     if let Some(plugin) = plugin.start().await? {
         plugin.join().await
     } else {
