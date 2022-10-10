24f535f8bc2f235f85ad4e93a07f2746ba20cea8
Christian Decker <decker.christian@gmail.com>
2022-10-10 16:29:11 +0200

    testing: Re-enable multisigner support

--- tools/glcli/glcli/cli.py
+++ tools/glcli/glcli/cli.py
@@ -16,7 +16,6 @@ import struct
 import sys
 import threading
 import time
-from filelock import FileLock
 
 
 logger = logging.getLogger("glapi.cli")
@@ -48,9 +47,6 @@ class Signer:
     def __init__(self, tls: Tls, network='testnet'):
         """Initialize the signer based on the cwd
         """
-        self.lock = FileLock("hsm_secret.lock")
-
-        self.lock.acquire()
         secrets_file = Path("hsm_secret")
         if not secrets_file.exists():
             logger.info(f"No {secrets_file} file found, generating a new secret key")
