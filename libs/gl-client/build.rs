17cff058315f29f22d1c17484f45f6475368283c
Christian Decker <decker.christian@gmail.com>
2022-11-03 14:22:39 +0100

    py: Migrate `get_info` to use the `cln-grpc` interface
    Now that we have a way to call a generic service we can simply switch
    over when needed.

--- libs/gl-client/build.rs
+++ libs/gl-client/build.rs
@@ -8,8 +8,13 @@ fn main() {
     }
 
     builder
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
-            &["../proto/greenlight.proto", "../proto/scheduler.proto"],
+            &[
+		"../proto/greenlight.proto",
+		"../proto/scheduler.proto",
+		"../proto/node.proto"
+	    ],
             &["../proto"],
         )
         .unwrap();
