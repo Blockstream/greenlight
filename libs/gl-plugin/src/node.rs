68a98194df3e5f5969700f6095b3b5415d7846bf
Christian Decker <decker.christian@gmail.com>
2022-11-17 16:22:59 +0100

    plugin: Defer RPC calls until the JSON-RPC socket is ready
    The plugin grpc interface has to start up before the JSON-RPC socket
    is available, otherwise the signer could not intervene to complete the
    startup. However that also means that early grpc calls that translate
    to JSON-RPC calls would end up not finding the socket. This wrapper
    wraps the cln-grpc service and waits for the socket file to appear
    before forwarding the call.

--- libs/gl-plugin/src/node.rs
+++ libs/gl-plugin/src/node.rs
@@ -765,15 +765,21 @@ use nix::unistd::getppid;
 impl PluginNodeServer {
     pub async fn run(self) -> Result<()> {
         let addr = self.grpc_binding.parse().unwrap();
+
+        let cln_node = NodeServer::new(
+            cln_grpc::Server::new(&self.rpc_path)
+                .await
+                .context("creating NodeServer instance")?,
+        );
+
         let router = tonic::transport::Server::builder()
             .tcp_keepalive(Some(tokio::time::Duration::from_secs(1)))
             .tls_config(self.tls.clone())?
             .layer(SignatureContextLayer::default())
-            .add_service(NodeServer::new(
-                cln_grpc::Server::new(&self.rpc_path)
-                    .await
-                    .context("creating NodeServer instance")?,
-            ))
+            .add_service(RpcWaitService {
+                inner: cln_node,
+                rpc_path: self.rpc_path.clone(),
+            })
             .add_service(crate::pb::node_server::NodeServer::new(self.clone()));
 
         info!("Starting grpc server on {}", addr);
@@ -913,4 +919,64 @@ where
             }
         })
     }
+}
+
+/// The RPC socket will not be available right away, so we wrap the
+/// cln-grpc service with this `Service` which essentially checks for
+/// the file's existence, and if it doesn't exist we wait for up to 5
+/// seconds for it to appear.
+#[derive(Debug, Clone)]
+struct RpcWaitService<S> {
+    rpc_path: std::path::PathBuf,
+    inner: S,
+}
+
+impl<S> Service<hyper::Request<hyper::Body>> for RpcWaitService<S>
+where
+    S: Service<hyper::Request<hyper::Body>, Response = hyper::Response<tonic::body::BoxBody>>
+        + Clone
+        + Send
+        + 'static,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = futures::future::BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, request: hyper::Request<hyper::Body>) -> Self::Future {
+        // This is necessary because tonic internally uses `tower::buffer::Buffer`.
+        // See https://github.com/tower-rs/tower/issues/547#issuecomment-767629149
+        // for details on why this is necessary
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+        let path = self.rpc_path.clone();
+        Box::pin(async move {
+            let deadline = tokio::time::Instant::now() + tokio::time::Duration::from_secs(5);
+            loop {
+                if deadline < tokio::time::Instant::now() {
+                    // Break and let it fail in the `inner.call`
+                    warn!("Deadline reached, letting the call fail");
+                    break;
+                }
+                match path.metadata() {
+                    Ok(_) => break,
+                    Err(_) => tokio::time::sleep(tokio::time::Duration::from_millis(500)).await,
+                }
+            }
+            inner.call(request).await
+        })
+    }
+}
+
+impl<S> tonic::transport::NamedService for RpcWaitService<S> {
+    // Well, this is cheating a bit, since we'll only ever wrap the
+    // cln.Node we can have this fixed.
+    const NAME: &'static str = "cln.Node";
 }
