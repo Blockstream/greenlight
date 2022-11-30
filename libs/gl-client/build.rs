5b6cf825c40b1d74430aa3e0702bdca4f062d65f
Riccardo Casatta <riccardo@casatta.it>
2022-11-30 11:31:38 +0100

    remove now unused serde attributes
    They are unused at the moment (the feature doesn't exist anymore)
    And they will be provided by the cln_rpc/cln_grpc migration

--- libs/gl-client/build.rs
+++ libs/gl-client/build.rs
@@ -1,69 +1,15 @@
 fn main() {
-    let mut builder = tonic_build::configure();
-
-    if cfg!(feature = "serde") {
-        for object in OBJECTS {
-            builder = builder.type_attribute(object, ATTR);
-        }
-    }
+    let builder = tonic_build::configure();
 
     builder
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
             &[
-		"../proto/greenlight.proto",
-		"../proto/scheduler.proto",
-		"../proto/node.proto"
-	    ],
+                "../proto/greenlight.proto",
+                "../proto/scheduler.proto",
+                "../proto/node.proto",
+            ],
             &["../proto"],
         )
         .unwrap();
 }
-
-const ATTR: &'static str = "#[derive(serde::Serialize, serde::Deserialize)]";
-const OBJECTS: [&'static str; 44] = [
-    "ConnectRequest",
-    "ConnectResponse",
-    "DisconnectRequest",
-    "DisconnectResponse",
-    "Outpoint",
-    "Confirmation",
-    "Amount.unit",
-    "Amount",
-    "FundChannelRequest",
-    "FundChannelResponse",
-    "Address",
-    "Feerate",
-    "Feerate.value",
-    "GetInfoRequest",
-    "GetInfoResponse",
-    "Invoice",
-    "InvoiceRequest",
-    "KeysendRequest",
-    "TlvField",
-    "Routehint",
-    "RoutehintHop",
-    "ListFundsRequest",
-    "ListFundsResponse",
-    "ListFundsChannel",
-    "ListFundsOutput",
-    "ListInvoicesRequest",
-    "InvoiceIdentifier",
-    "InvoiceIdentifier.id",
-    "ListInvoicesResponse",
-    "ListPaymentsRequest",
-    "PaymentIdentifier",
-    "PaymentIdentifier.id",
-    "ListPaymentsResponse",
-    "Payment",
-    "ListPeersRequest",
-    "ListPeersResponse",
-    "Peer",
-    "Channel",
-    "Htlc",
-    "NewAddrRequest",
-    "NewAddrResponse",
-    "PayRequest",
-    "WithdrawRequest",
-    "WithdrawResponse",
-];
