b735605daed63f4f9d9e02281b807248874f09c9
Peter Neuroth <pet.v.ne@gmail.com>
2023-05-24 21:50:54 +0200

    gl-client: Add scheduler builder
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- libs/gl-client/src/scheduler.rs
+++ libs/gl-client/src/scheduler.rs
@@ -3,11 +3,93 @@ use crate::tls::{self, TlsConfig};
 
 use crate::node::GrpcClient;
 use crate::{node, pb, signer::Signer, utils};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use lightning_signer::bitcoin::Network;
 use tonic::transport::Channel;
 
 type Client = SchedulerClient<Channel>;
+
+/// Builds [`Scheduler`] with custom configuration values and creates the grpc
+/// client for the `Scheduler`.
+/// Methods can be chained in order to set configuration values.
+/// The [`Scheduler`] is constructed and connected to the backend by calling
+/// `connect`.
+/// A new `SchedulerBuilder` instance is obtained via
+/// [`SchedulerBuilder::new()`]
+/// or by the `Scheduler` via
+/// [`Scheduler::builder()`].
+///
+/// `node_id` and `network` are mandatory configuration parameters.
+///
+/// # Examples
+///
+/// ```
+/// use lightning_signer::bitcoin::Network;
+/// let scheduler = SchedulerBuilder::new(vec![0u8; 32], Network::Testnet)
+///     .uri("https://my.gl-scheduler.uri.com")?
+///     .connect()
+///     .await?;
+/// ```
+pub struct SchedulerBuilder {
+    node_id: Vec<u8>,
+    network: Network,
+    tls: Option<TlsConfig>,
+    uri: Option<String>,
+}
+
+impl SchedulerBuilder {
+    /// Creates a new SchedulerBuilder
+    pub fn new(node_id: Vec<u8>, network: Network) -> SchedulerBuilder {
+        SchedulerBuilder {
+            node_id,
+            network,
+            tls: None,
+            uri: None,
+        }
+    }
+
+    /// Sets the `TlsConfig` that the `Scheduler` uses to connect to the
+    /// greenlight backend. If unset defaults to the default `TlsConfig`.
+    pub fn tls_config<'a>(&'a mut self, tls: &TlsConfig) -> Result<&'a mut SchedulerBuilder> {
+        if self.tls.is_some() {
+            return Err(anyhow!("tls config already set"));
+        }
+        self.tls = Some(tls.clone());
+        Ok(self)
+    }
+
+    /// Sets the uri that the `Scheduler` uses to connect to the greenlight
+    /// backend. If unset defaults to the builtin in default uri.
+    pub fn uri<'a>(&'a mut self, uri: String) -> Result<&'a mut SchedulerBuilder> {
+        if self.uri.is_some() {
+            return Err(anyhow!("uri already set"));
+        }
+        self.uri = Some(uri);
+        Ok(self)
+    }
+
+    /// Builds the `Scheduler` ready with the grpc-`Client` connected to the
+    /// greenlight backend.
+    pub async fn connect(&self) -> Result<Scheduler> {
+        let uri = self.uri.clone().unwrap_or_else(|| utils::scheduler_uri());
+        let tls = match self.tls.clone() {
+            Some(tls) => tls,
+            None => crate::tls::TlsConfig::new()?,
+        };
+        debug!("Connection to scheduler at {}", uri);
+        let channel = Channel::from_shared(uri)?
+            .tls_config(tls.inner.clone())?
+            .connect()
+            .await?;
+
+        let client = SchedulerClient::new(channel);
+        Ok(Scheduler {
+            node_id: self.node_id.clone(),
+            client,
+            network: self.network,
+        })
+    }
+}
 
 #[derive(Clone)]
 pub struct Scheduler {
@@ -17,31 +99,31 @@ pub struct Scheduler {
 }
 
 impl Scheduler {
+    /// Returns the builder for a `Scheduler`. See [`SchedulerBuilder`] for
+    /// details on how to use the builder.
+    pub fn builder(node_id: Vec<u8>, network: Network) -> SchedulerBuilder {
+        SchedulerBuilder::new(node_id, network)
+    }
+
+    #[deprecated = "users should instead use `Scheduler::builder(node_id: Vec<u8>, network: Network)`"]
     pub async fn with(
         node_id: Vec<u8>,
         network: Network,
         uri: String,
         tls: &TlsConfig,
     ) -> Result<Scheduler> {
-        debug!("Connecting to scheduler at {}", uri);
-        let channel = Channel::from_shared(uri)?
-            .tls_config(tls.inner.clone())?
+        let scheduler = Self::builder(node_id, network)
+            .uri(uri)?
+            .tls_config(tls)?
             .connect()
             .await?;
-
-        let client = SchedulerClient::new(channel);
-
-        Ok(Scheduler {
-            client,
-            node_id,
-            network,
-        })
+        Ok(scheduler)
     }
 
+    #[deprecated = "users should instead use `Scheduler::builder(node_id: Vec<u8>, network: Network)`"]
     pub async fn new(node_id: Vec<u8>, network: Network) -> Result<Scheduler> {
-        let tls = crate::tls::TlsConfig::new()?;
-        let uri = utils::scheduler_uri();
-        Self::with(node_id, network, uri, &tls).await
+        let scheduler = Self::builder(node_id, network).connect().await?;
+        Ok(scheduler)
     }
 
     pub async fn register(
