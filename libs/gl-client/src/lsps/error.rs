980f6c5399d3b1709592ab6db426dbe7daa5532e
Erik De Smedt <edesmedt@blockstream.com>
2023-09-15 09:29:55 +0200

    json-rpc error-codes

--- libs/gl-client/src/lsps/error.rs
+++ libs/gl-client/src/lsps/error.rs
@@ -24,15 +24,25 @@ impl From<std::io::Error> for LspsError {
     }
 }
 
-
-fn map_json_rpc_error_code_to_str(code : i64) -> &'static str {
+pub fn map_json_rpc_error_code_to_str(code : i64) -> &'static str {
     match code {
         -32700 => "parsing_error",
         -32600 => "invalid_request",
         -32601 => "method_not_found",
         -32602 => "invalid_params",
         -32603 => "internal_error",
-        -32099..=-32000 => "implementation_defined_server_error"
+        -32099..=-32000 => "implementation_defined_server_error",
         _ => "unknown_error_code"
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::lsps::error::map_json_rpc_error_code_to_str;
+
+    #[test]
+    fn test_map_json_rpc_error_code_to_str() {
+        assert_eq!(map_json_rpc_error_code_to_str(12), "unknown_error_code");
+        assert_eq!(map_json_rpc_error_code_to_str(-32603), "internal_error");
     }
 }
