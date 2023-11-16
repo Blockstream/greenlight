b5db9d19ad61539480ab74004a1258b0d49b32e9
Christian Decker <decker.christian@gmail.com>
2023-11-16 13:39:25 +0100

    py: Add FetchInvoice and Wait, and clean up
    We also try to get mypy working :-)

--- libs/gl-client-py/glclient/greenlight_pb2.py
+++ libs/gl-client-py/glclient/greenlight_pb2.py
@@ -19,7 +19,6 @@ _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'greenlight_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
   _globals['_NETADDRESSTYPE']._serialized_start=5843
   _globals['_NETADDRESSTYPE']._serialized_end=5901
--- libs/gl-client-py/glclient/scheduler_pb2.py
+++ libs/gl-client-py/glclient/scheduler_pb2.py
@@ -20,7 +20,6 @@ _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'scheduler_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
   _UPGRADEREQUEST.fields_by_name['initmsg']._options = None
   _UPGRADEREQUEST.fields_by_name['initmsg']._serialized_options = b'\030\001'
