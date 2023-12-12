b0de09fef03c85ccadb49c36fd16fe370c68116e
Peter Neuroth <pet.v.ne@gmail.com>
2023-12-12 14:13:25 +0200

    pairing: Add check for the PairingDataResponse
    This adds a method to the pairing service that allows to check the
    validity of the PairingDataResponse. This is that the public key from
    the session_id matches the public key from the CSR.
    
    We may want to add a signature for the restrictions to ensure that GL
    did not manipulate them. We are OK for now as a user has to aggree to
    the restrictions anyway.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- libs/gl-client-py/tests/test_pairing.py
+++ libs/gl-client-py/tests/test_pairing.py
@@ -50,3 +50,33 @@ def test_pairing_session(attestation_device, creds):
     assert(m.device_key)
     # assert(m.rune) fixme: enable once we pass back a rune during the tests.
     assert(m.creds)
+
+
+def test_paring_data_validation(attestation_device, creds):
+    """A simple test to ensure that data validation works as intended.
+
+    If the data is valid, the public key belongs to the private key that was
+    used to sign the csr subject.
+    """
+    name = "new_device"
+    desc = "my description"
+    restrs = "method^list"
+
+    dc = NewDeviceClient(TlsConfig(creds))
+    session = dc.pair_device(name, desc, restrs)
+    session_iter = iter(session)
+    m = next(session_iter)
+    session_id = m.data.split(':')[1]
+
+    ac = AttestationDeviceClient(creds=attestation_device.creds())
+
+    # check for pairing data.
+    session_id = m.data.split(':')[1]
+    m = ac.get_pairing_data(session_id)
+    
+    assert(ac.verify_pairing_data(m) is None)
+
+    # Change the public key and try again
+    pk = '01' + m.session_id[2:] if m.session_id[0:1] == '00' else '00' + m.session_id[2:]
+    m.session_id = pk
+    assert(ac.verify_pairing_data(m))
