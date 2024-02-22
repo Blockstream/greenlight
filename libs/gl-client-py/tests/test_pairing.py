0b6d46d522345c78ece79f988de7fb1c7f6eae79
Peter Neuroth <pet.v.ne@gmail.com>
2024-02-22 15:23:14 +0100

    pairing: Add client side pairing session init
    This implements the service and the methods to initialize a pairing
    session on the "new device"
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

---
+++ libs/gl-client-py/tests/test_pairing.py
@@ -0,0 +1,14 @@
+from fixtures import *
+from glclient.pairing import NewDeviceClient
+from glclient.tls import TlsConfig
+
+def test_pairing_session(scheduler, nobody_id):
+    ps = NewDeviceClient(TlsConfig())
+    session = ps.pair_device("new_device", "my_description", "")
+    session_iter = iter(session)
+    m = next(session_iter)
+    assert(m.device_id)
+    assert(m.device_cert)
+    assert(m.device_key)
+    # assert(m.rune) fixme: enable once we pass back a rune during the tests.
+    assert(m.creds)
