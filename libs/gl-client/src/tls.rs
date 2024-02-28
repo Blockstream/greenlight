fb2bfc6b0f6d144ac0dfd0e19af08873eb38741b
randy808 <rnd.naar@gmail.com>
2024-02-28 06:44:01 +0100

    Added validation that checks whether the node_id the Scheduler was initialized with matches the node_id extracted from the TlsConfig passed in to the 'schedule' call.

--- libs/gl-client/src/tls.rs
+++ libs/gl-client/src/tls.rs
@@ -1,7 +1,8 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use log::debug;
 use std::path::Path;
 use tonic::transport::{Certificate, ClientTlsConfig, Identity};
+use x509_certificate::X509Certificate;
 
 const CA_RAW: &[u8] = include_str!("../../tls/ca.pem").as_bytes();
 const NOBODY_CRT: &[u8] = include_str!(env!("GL_NOBODY_CRT")).as_bytes();
@@ -23,6 +24,11 @@ pub struct TlsConfig {
     pub(crate) private_key: Option<Vec<u8>>,
 
     pub ca: Vec<u8>,
+
+    /// The device_crt parsed as an x509 certificate. Used to
+    /// validate the common subject name against the node_id
+    /// configured on the scheduler.
+    pub x509_cert: Option<X509Certificate>,
 }
 
 fn load_file_or_default(varname: &str, default: &[u8]) -> Result<Vec<u8>> {
@@ -44,6 +50,13 @@ impl TlsConfig {
         Self::with(nobody_crt, nobody_key, ca_crt)
     }
     pub fn with<V: AsRef<[u8]>>(crt: V, key: V, ca_crt: V) -> Result<Self> {
+        let x509_cert = match X509Certificate::from_pem(crt.as_ref()) {
+            Ok(x) => x,
+            Err(e) => {
+                return Err(anyhow!("Failed to parse x509 certificate: {}", e));
+            }
+        };
+
         let config = ClientTlsConfig::new()
             .ca_certificate(Certificate::from_pem(ca_crt.as_ref()))
             .identity(Identity::from_pem(crt, key.as_ref()));
@@ -52,6 +65,7 @@ impl TlsConfig {
             inner: config,
             private_key: Some(key.as_ref().to_vec()),
             ca: ca_crt.as_ref().to_vec(),
+            x509_cert: Some(x509_cert),
         })
     }
 }
@@ -64,9 +78,12 @@ impl TlsConfig {
     /// nodes. If the `TlsConfig` is not upgraded, nodes will reply
     /// with handshake failures, and abort the connection attempt.
     pub fn identity(self, cert_pem: Vec<u8>, key_pem: Vec<u8>) -> Self {
+        let x509_cert = X509Certificate::from_pem(&cert_pem);
+
         TlsConfig {
-            inner: self.inner.identity(Identity::from_pem(cert_pem, &key_pem)),
+            inner: self.inner.identity(Identity::from_pem(&cert_pem, &key_pem)),
             private_key: Some(key_pem),
+            x509_cert: x509_cert.map_or(None, |x| Some(x)),
             ..self
         }
     }
