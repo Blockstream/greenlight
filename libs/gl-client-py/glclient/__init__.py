7d91832b1f0ebc48e12b056f45f16756581d4bf3
Christian Decker <decker.christian@gmail.com>
2024-06-05 16:40:00 +0200

    fixup! Upgraded greenlight core lightning version to 24.02.1.

--- libs/gl-client-py/glclient/__init__.py
+++ libs/gl-client-py/glclient/__init__.py
@@ -17,7 +17,7 @@ backup_decrypt_with_seed = native.backup_decrypt_with_seed
 
 
 # Keep in sync with the libhsmd version, this is tested in unit tests.
-__version__ = "v23.08"
+__version__ = "v24.02"
 
 
 E = TypeVar('E', bound=PbMessage)
