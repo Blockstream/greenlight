85de55b4abe1b4e7256d8cc933d24890c23e82ad
Peter Neuroth <pet.v.ne@gmail.com>
2024-07-08 18:06:03 +0200

    gl-plugin: Annotate TrampolinePayRequest in proto
    This commit adds a derive to the TrampolinePayRequest in order to be
    able to serialize and deserialize it via serde. This allows to use those
    messages as json::Value's directly.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- libs/gl-plugin/build.rs
+++ libs/gl-plugin/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tonic_build::compile_protos("../proto/glclient/greenlight.proto").unwrap();
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .type_attribute("TrampolinePayRequest", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(
+            &["../proto/glclient/greenlight.proto"],
+            &["../proto/glclient"],
+        )
+        .unwrap();
 }
