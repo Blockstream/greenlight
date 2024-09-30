935addd6a764df828359d529f55c5e99a163a552
Peter Neuroth <pet.v.ne@gmail.com>
2024-09-30 17:11:27 +0200

    glcli: Add listpays method
    Adding listpays requires us to decode the input hex string.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- libs/gl-cli/src/node.rs
+++ libs/gl-cli/src/node.rs
@@ -77,6 +77,17 @@ pub enum Command {
         #[arg(help = "The peer's port number defaults to the networks default ports if missing.")]
         port: Option<u32>,
     },
+    /// List attempted payments
+    Listpays {
+        #[arg(help = "A Bolt11 string to get the payment details")]
+        bolt11: Option<String>,
+        #[arg(help = "A payment_hash to get the payment details")]
+        payment_hash: Option<String>,
+        #[arg(
+            help = "Can be one of \"pending\", \"completed\", \"failed\", filters the payments that are returned"
+        )]
+        status: Option<String>,
+    },
     /// Stop the node
     Stop,
 }
@@ -140,6 +151,38 @@ pub async fn command_handler<P: AsRef<Path>>(cmd: Command, config: Config<P>) ->
             .await
         }
         Command::Connect { id, host, port } => connect_handler(config, id, host, port).await,
+        Command::Listpays {
+            bolt11,
+            payment_hash,
+            status,
+        } => {
+            let payment_hash = if let Some(v) = payment_hash {
+                match hex::decode(v) {
+                    Ok(decoded) => Some(decoded),
+                    Err(e) => {
+                        println!("Payment hash is not a valid hex string: {}", e);
+                        return Ok(()); // Exit the function early if hex decoding fails
+                    }
+                }
+            } else {
+                None
+            };
+            let status = if let Some(status_str) = status {
+                match status_str.as_str() {
+                    "pending" => Some(0),
+                    "complete" => Some(1),
+                    "failed" => Some(2),
+                    _ => {
+                        println!("Invalid status: {}, expected one of \"pending\", \"completed\", \"failed\"", status_str);
+                        return Ok(()); // Exit the function early if the status is invalid
+                    }
+                }
+            } else {
+                None
+            };
+
+            listpays_handler(config, bolt11, payment_hash, status).await
+        }
         Command::Stop => stop(config).await,
     }
 }
@@ -309,6 +352,41 @@ async fn connect_handler<P: AsRef<Path>>(
     let mut node: gl_client::node::ClnClient = scheduler.node().await.map_err(Error::custom)?;
     let res = node
         .connect_peer(cln::ConnectRequest { id, host, port })
+        .await
+        .map_err(|e| Error::custom(e.message()))?
+        .into_inner();
+    println!("{:?}", res);
+    Ok(())
+}
+
+async fn listpays_handler<P: AsRef<Path>>(
+    config: Config<P>,
+    bolt11: Option<String>,
+    payment_hash: Option<Vec<u8>>,
+    status: Option<i32>,
+) -> Result<()> {
+    let creds_path = config.data_dir.as_ref().join(CREDENTIALS_FILE_NAME);
+    let creds = match util::read_credentials(&creds_path) {
+        Some(c) => c,
+        None => {
+            return Err(Error::CredentialsNotFoundError(format!(
+                "could not read from {}",
+                creds_path.display()
+            )))
+        }
+    };
+
+    let scheduler = gl_client::scheduler::Scheduler::new(config.network, creds)
+        .await
+        .map_err(Error::custom)?;
+
+    let mut node: gl_client::node::ClnClient = scheduler.node().await.map_err(Error::custom)?;
+    let res = node
+        .list_pays(cln::ListpaysRequest {
+            bolt11,
+            payment_hash,
+            status,
+        })
         .await
         .map_err(|e| Error::custom(e.message()))?
         .into_inner();
