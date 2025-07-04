d6e6c350d3c17139d7344afbc319fdf1db50b1ec
Christian Decker <decker.christian@gmail.com>
2025-07-04 16:26:10 +0200

    signer: Bump the signer version to trigger a node upgrade

--- libs/gl-client-py/glclient/__init__.py
+++ libs/gl-client-py/glclient/__init__.py
@@ -17,7 +17,7 @@ backup_decrypt_with_seed = native.backup_decrypt_with_seed
 
 
 # Keep in sync with the libhsmd version, this is tested in unit tests.
-__version__ = "v24.11"
+__version__ = "v25.05"
 
 
 E = TypeVar("E", bound=PbMessage)
