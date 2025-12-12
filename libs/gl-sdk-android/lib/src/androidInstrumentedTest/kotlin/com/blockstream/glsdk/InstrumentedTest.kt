75c6362632fac8221eb09d066ef983e1d08b1b5a
Angelos Veglektsis <angelos@aveworks.com>
2025-12-12 14:20:59 +0200

    fix: android bindings

---
+++ libs/gl-sdk-android/lib/src/androidInstrumentedTest/kotlin/com/blockstream/glsdk/InstrumentedTest.kt
@@ -0,0 +1,21 @@
+package com.blockstream.glsdk
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class InstrumentedTest {
+
+    @Test
+    fun test_signer() {
+        val mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+        val signer = Signer(mnemonic)
+        signer.start()
+    }
+}
