fa54a4010a7c769a37706db4fed09806376c3fd2
Christian Decker <decker.christian@gmail.com>
2025-12-17 16:53:37 +0100

    sdk: Fix two issues with the signer
    `Signer.start()` was not creating the runtime, and spawning the
    thread, rather it was assuming it already had a runtime. Since the
    signer is isolated, we can just have it spawn its own thread and
    runtime. The API will use a shared runtime it can block on, but the
    signer needs to run independently in the background.
    
    The second issue was that the `Signer.node_id()` method was still
    stubbed out. Now it delegates to the gl-client Signer.
    
    Reported-By: <@lvaccaro>

--- libs/gl-sdk/tests/test_scheduler.py
+++ libs/gl-sdk/tests/test_scheduler.py
@@ -1,10 +1,16 @@
-"""Testing client-scheduler interactions
-"""
+"""Testing client-scheduler interactions"""
 
 import pytest
 import glsdk
 from gltesting.fixtures import *
 
+
 def test_register(scheduler, clients):
-    signer = glsdk.Signer("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
-    
+    signer = glsdk.Signer(" ".join(["abandon"] * 11 + ["about"]))
+    signer.start()
+
+    # Check that the derived node_id matches the Signer seed phrase.
+    assert (
+        signer.node_id().hex()
+        == "03653e90c1ce4660fd8505dd6d643356e93cfe202af109d382787639dd5890e87d"
+    )
