e0552d379eec3fdb42830c90cc38312865e68d4d
ShahanaFarooqui <shahana.farooqui@gmail.com>
2026-03-02 21:40:28 -0800

    gl-sdk-napi: Update tests to run with local gltestserver

--- libs/gl-sdk-napi/tests/basic.spec.ts
+++ libs/gl-sdk-napi/tests/basic.spec.ts
@@ -1,13 +1,14 @@
-import { describe, it, expect } from '@jest/globals';
+import * as crypto from 'crypto';
+import * as bip39 from 'bip39';
 import { Credentials, Scheduler, Signer, Node } from '../index.js';
-
-const MNEMONIC = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
 
 describe('Greenlight node', () => {
   let node: Node;
   let credentials: Credentials;
 
   it('can be setup', async () => {
+    const rand: Buffer = crypto.randomBytes(16);
+    const MNEMONIC: string = bip39.entropyToMnemonic(rand.toString("hex"));
     const scheduler = new Scheduler('regtest');
     const signer = new Signer(MNEMONIC);
     const nodeId = signer.nodeId();
@@ -15,7 +16,7 @@ describe('Greenlight node', () => {
     expect(Buffer.isBuffer(nodeId)).toBe(true);
     expect(nodeId.length).toBeGreaterThan(0);
 
-    credentials = await scheduler.recover(signer);
+    credentials = await scheduler.register(signer);
     node = new Node(credentials);
     expect(node).toBeTruthy();
   });
